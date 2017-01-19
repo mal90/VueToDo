9ac4fffd216cc009e374812a7839ff76ec900917
Salim Malik <SalimM@synergenhealth.com>
2017-01-19 09:25:53 -0600

    bind event and add event

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 // app.js
-
+Vue.config.debug = true;
+Vue.config.devtools = true;
 new Vue({
-// We want to target the div with an id of 'events'
-  el: '#events'
+  // We want to target the div with an id of 'events'
+    el: '#events',
+
+    // Here we can register any values or collections that hold data
+    // for the application
+    data: {
+      event: { name: '', description: '', date: '' },
+      events: []
+    },
+
+    // Anything within the ready function will run when the application loads
+    ready: function() {},
+
+    // Methods we want to use in our application are registered here
+    methods: {
+      addEvent: function()
+      {
+        if(this.event.name)
+        {
+          this.events.push(this.event);
+          this.event = { name: '', description: '', date: '' };
+        }
+      }
+    }
 });
