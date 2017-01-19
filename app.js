7ec2bee26c4a22d15993d90a6dfdfe852455d26a
Salim Malik <SalimM@synergenhealth.com>
2017-01-19 09:47:26 -0600

    Rename title

--- app.js
+++ app.js
@@ -23,38 +23,41 @@ new Vue({
     // Methods we want to use in our application are registered here
     methods: {
       // We dedicate a method to retrieving and setting some data
-  fetchEvents: function() {
-    var events = [
-        {
-          id: 1,
-          name: 'TIFF',
-          description: 'Toronto International Film Festival',
-          date: '2015-09-10'
-        },
-        {
-          id: 2,
-          name: 'The Martian Premiere',
-          description: 'The Martian comes to theatres.',
-          date: '2015-10-02'
-        },
-        {
-          id: 3,
-          name: 'SXSW',
-          description: 'Music, film and interactive festival in Austin, TX.',
-          date: '2016-03-11'
-        }
-      ];
-      // $set is a convenience method provided by Vue that is similar to pushing
-      // data onto an array
-        this.$set('events', events);
-      },
-      
+        fetchEvents: function() {
+          var events = [
+              {
+                id: 1,
+                name: 'TIFF',
+                description: 'Toronto International Film Festival',
+                date: '2015-09-10'
+              },
+              {
+                id: 2,
+                name: 'The Martian Premiere',
+                description: 'The Martian comes to theatres.',
+                date: '2015-10-02'
+              },
+              {
+                id: 3,
+                name: 'SXSW',
+                description: 'Music, film and interactive festival in Austin, TX.',
+                date: '2016-03-11'
+              }
+            ];
+            // $set is a convenience method provided by Vue that is similar to pushing
+            // data onto an array
+              this.$set('events', events);
+            },
+
       addEvent: function()
       {
         if(this.event.name)
         {
           this.events.push(this.event);
           this.event = { name: '', description: '', date: '' };
+        }
+        else{
+          alert("Please add a name to the event!");
         }
       }
     }
