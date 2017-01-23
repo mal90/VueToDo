02f74059096e2ce31faa41490fa18f1118a63221
Salim Malik <madkonline@gmail.com>
2017-01-23 10:17:33 -0600

    Refactor code

--- app.js
+++ app.js
@@ -2,52 +2,41 @@
 Vue.config.debug = true;
 Vue.config.devtools = true;
 new Vue({
-  // We want to target the div with an id of 'events'
-    el: '#events',
+    el: '#events', //target element is div with id=element
 
-    // Here we can register any values or collections that hold data
-    // for the application
     data: {
-      event: { name: '', description: '', date: '' },
+      event: { name: '', description: '', date: '' }, //"data" is similer to scope in AngularJs . It is the glue between ctrl and view
       events: []
     },
 
-    // Anything within the ready function will run when the application loads
-    ready: function() {
-      // When the application loads, we want to call the method that initializes
-      // some data
+    ready: function() { //ready is the init method . runs when vue initializes
       this.fetchEvents();
     },
 
-
-    // Methods we want to use in our application are registered here
-    methods: {
-      // We dedicate a method to retrieving and setting some data
+    methods: { //this is where we register methods
         fetchEvents: function() {
           var events = [
               {
                 id: 1,
-                name: 'TIFF',
-                description: 'Toronto International Film Festival',
+                name: 'Do shopping',
+                description: 'Buy monthly groceries from Keels',
                 date: '2015-09-10'
               },
               {
                 id: 2,
-                name: 'The Martian Premiere',
-                description: 'The Martian comes to theatres.',
+                name: 'Take the motorcycle to service',
+                description: 'Change new tires',
                 date: '2015-10-02'
               },
               {
                 id: 3,
-                name: 'SXSW',
-                description: 'Music, film and interactive festival in Austin, TX.',
+                name: 'Take out garbage',
+                description: 'And clean the garden',
                 date: '2016-03-11'
               }
             ];
-            // $set is a convenience method provided by Vue that is similar to pushing
-            // data onto an array
               this.$set('events', events);
-            },
+      },
 
       addEvent: function()
       {
@@ -61,10 +50,9 @@ new Vue({
         }
       },
 
-      deleteEvent: function(x) {
+      deleteEvent: function(item) {
         if(confirm("Are you sure you want to delete this event?")) {
-          // $remove is a Vue convenience method similar to splice
-          this.events.$remove(x);
+          this.events.$remove(item); // $remove is a Vue convenience method similar to splice
         }
       }
     }
